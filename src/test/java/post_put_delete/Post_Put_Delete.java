d82b4adc1f51399b6512178652265aec046b5a56
AbhilashGUI <vemulaabhilash8433@gmail.com>
2026-08-18 15:09:00 +0530

    Online project learning

--- src/test/java/post_put_delete/Post_Put_Delete.java
+++ src/test/java/post_put_delete/Post_Put_Delete.java
@@ -12,78 +12,103 @@ import static org.hamcrest.Matchers.*;
 import java.io.File;
 public class Post_Put_Delete {
 
-        @Test
-        public void post_request() {
+    @Test
+    public void post_request() {
+        File file = new File("create_employee.json");
+        Integer id = given()
+                .baseUri("https://dummy.restapiexample.com/api/v1")
+                .basePath("/create")
+                .contentType(ContentType.JSON)
+                .body(file)
+                .when()
+                .post()
+                .then()
+                .statusCode(200)
+                .body("data.age", equalTo("23"))
+                .extract().path("data.id");
+        System.out.println(id);
 
-            File file = new File("create_employee.json");
 
-            int id = given()
-                    .baseUri("http://dummy.restapiexample.com/api/v1")
-                    .contentType(ContentType.JSON)
-                    .body(file).
-                    when()
-                    .post("/create").
-                    then()
-                    .statusCode(200)
-                    .body("data.name", equalTo("Mark"))
-                    .extract().path("data.id");
-            System.out.println(id);
-        }
+    }
 
-        @Test
-        public void post_request_using_json_object() {
+    @Test
+    public void post_request_using_json_object() {
 
-            JSONObject body = new JSONObject();
-            body.put("name", "Ashton Cox");
-            body.put("salary", "86000");
-            body.put("age", 66);
+        JSONObject body = new JSONObject();
+        body.put("name", "Abhilash");
+        body.put("salary", "70000");
+        body.put("age", "32");
 
-            int id = given()
-                    .baseUri("http://dummy.restapiexample.com/api/v1")
-                    .contentType(ContentType.JSON)
-                    .body(body.toString()).
-                    when()
-                    .post("/create").
-                    then()
-                    .statusCode(200)
-                    .body("data.name", equalTo("Ashton Cox"))
-                    .extract().path("data.id");
+        Integer id = given()
+                .baseUri("https://dummy.restapiexample.com/api/v1")
+                .basePath("/create")
+                .contentType(ContentType.JSON)
+                .body(body.toString())
+                .when()
+                .post()
+                .then().body("data.name", equalTo("Abhilash"))
+                .extract().path("data.id");
+        System.out.println(id);
 
-            System.out.println(id);
-        }
+    }
 
-        @Test
-        public void put_request_using_json_object() {
+    @Test
+    public void put_request_using_json_object() {
 
-            JSONObject body = new JSONObject();
-            body.put("name", "Tiger Nixon");
-            body.put("salary", "320800");
-            body.put("age", "61");
+        JSONObject body = new JSONObject();
+        body.put("employee_name", "Amith");
+        body.put("employee_salary", "180000");
+        body.put("employee_age", "32");
 
-            given()
-                    .baseUri("http://dummy.restapiexample.com/api/v1")
-                    .contentType(ContentType.JSON)
-                    .body(body.toString()).
-                    when()
-                    .put("/update/2").
-                    then()
-                    .statusCode(200);
-        }
-
-        @Test
-        public void delete_request() {
-
-            String msg = given()
-                    .baseUri("http://dummy.restapiexample.com/api/v1")
-                    .contentType(ContentType.JSON).
-                    when()
-                    .delete("/delete/2").
-                    then()
-                    .statusCode(200)
-                    .extract().path("message");
-
-            System.out.println(msg);
-        }
+        given()
+                .baseUri("https://dummy.restapiexample.com/api/v1")
+                .basePath("/update/21")
+                .contentType(ContentType.JSON)
+                .body(body.toString())
+                .when()
+                .put()
+                .then().log().all()
+                .statusCode(200);
     }
 
 
+    @Test
+    public void get_request_check() {
+        given()
+                .baseUri("https://dummy.restapiexample.com/api/v1")
+                .basePath("/employee/21")
+                .when().get()
+                .then().log().all()
+                .statusCode(200);
+    }
+
+    //Since they are Dummy API's they will not behave as intended
+
+
+    @Test
+    public void delete_request() {
+
+        String message = given()
+                .baseUri("https://dummy.restapiexample.com/api/v1")
+                .basePath("/delete/21")
+                .when().delete()
+                .then().log().all()
+                .statusCode(200)
+                .extract().path("message");
+        System.out.println(message);
+    }
+
+
+    @Test
+    public void get_request_check2() {
+        given()
+                .baseUri("https://dummy.restapiexample.com/api/v1")
+                .basePath("/employee/21")
+                .when().get()
+                .then().log().all()
+                .statusCode(200);
+    }
+
+    //It retrieve the data , since they are dummy API's
+
+}
