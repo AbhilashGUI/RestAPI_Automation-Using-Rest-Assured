ea80b42b2f09c60d17f7065169937610ecc1990f
AbhilashGUI <vemulaabhilash8433@gmail.com>
2026-08-19 09:14:16 +0530

    Online project learning

--- src/test/java/request_parameters/Request_Parameters.java
+++ src/test/java/request_parameters/Request_Parameters.java
@@ -11,10 +11,12 @@ public class Request_Parameters {
     public void handling_query_parameters() {
 
         given()
-                .baseUri("https://restcountries.com/v3.1")
-                .queryParam("fullText", true)
+                .baseUri("https://restcountries.com/countries/v5")
+                .basePath("/name")
+                .queryParam("q", "India")
+                .headers("Authorization", "Bearer rc_live_8f6a48fb0c9445b0974351a9b19be393")
                 .when()
-                .get("/name/india")
+                .get()
                 .then()
                 .log().all()
                 .statusCode(200);
@@ -22,18 +24,21 @@ public class Request_Parameters {
     }
 
     @Test
-    public void handling_multiple_parameters() {
+    public void handling_multiple_query_parameters() {
 
-        HashMap<String,Object> params = new HashMap<String,Object>();
-        params.put("access_key", "eaaaa33d3571fef962d994f117f202dd");
-        params.put("Symbols", "INR");
+        HashMap<String,Object> params=new HashMap<>();
+        params.put("access_key","eaaaa33d3571fef962d994f117f202dd");
+        params.put("Symbols","INR");
 
         given()
-                    .baseUri("http://data.fixer.io/api")
+                .baseUri("http://data.fixer.io/api")
+                .basePath("latest")
                 .queryParams(params)
+                //.queryParam("access_key","eaaaa33d3571fef962d994f117f202dd")
+                //.queryParam("Symbols","INR")
                 .when()
-                .get("/latest").
-                then()
+                .get()
+                .then()
                 .log().all()
                 .statusCode(200);
 
@@ -42,11 +47,14 @@ public class Request_Parameters {
     @Test
     public void handling_multi_value_parameters() {
         given()
-                .baseUri("https://restcountries.com/v3.1")
-                .queryParam("codes", "col,no,ee,in").
-                when()
-                .get("/alpha").
-                then()
+                .baseUri("https://restcountries.com/countries/v5")
+                .basePath("/name")
+                .queryParams("q","alpha")
+                .headers("Authorization", "Bearer rc_live_8f6a48fb0c9445b0974351a9b19be393")
+                .queryParam("codes", "col,no,ee,in")
+                .when()
+                .get()
+                .then()
                 .log().all()
                 .statusCode(200);
 
@@ -57,11 +65,13 @@ public class Request_Parameters {
     @Test
     public void handling_path_parameter() {
         given()
-                .baseUri("https://restcountries.com/v3.1")
-                .pathParam("currency", "usd").
-                when()
-                .get("/currency/{currency}").
-                then()
+                .baseUri("https://restcountries.com/countries/v5")
+                .basePath("/currencies/{currency}")
+                .pathParam("currency", "usd")
+                .headers("Authorization", "Bearer rc_live_8f6a48fb0c9445b0974351a9b19be393")
+                .when()
+                .get()
+                .then()
                 .log().all()
                 .statusCode(200);
     }
@@ -76,12 +86,13 @@ public class Request_Parameters {
 
         given()
                 .baseUri("https://postman-echo.com")
+                .basePath("/post")
                 .contentType("application/x-www-form-urlencoded;charset=UTF-8")
                 .formParam("First Name", "Abhilash")
                 .formParam("Last Name", "Vemula").
                 when()
-                .post("/post").
-                then()
+                .post()
+                .then()
                 .log().all()
                 .statusCode(200);
 
--- src/test/java/rest_logging/Rest_Logging.java
+++ src/test/java/rest_logging/Rest_Logging.java
@@ -9,58 +9,70 @@ public class Rest_Logging {
     public void log_all_details() {
 
         given()
-                .baseUri("https://restcountries.com/v3.1").
-                when()
-                .get("/alpha/US").
-                then()
-                .log().all();
-
+                .baseUri("https://restcountries.com/countries/v5")
+                .basePath("/name")
+                .queryParam("q","USA")
+                .headers("Authorization", "Bearer rc_live_8f6a48fb0c9445b0974351a9b19be393")
+                .when().get()
+                .then().log().all();
 
     }
 
     @Test
-    public void log_body_or_header() {
+    public void log_body_and_header() {
         given()
-                .baseUri("https://restcountries.com/v3.1")
-                .when()
-                .get("/alpha/US").
-                then()
-                .log().headers();
-                //.log().body();
+                .baseUri("https://restcountries.com/countries/v5")
+                .basePath("/name")
+                .queryParam("q", "USA")
+                .headers("Authorization","Bearer rc_live_8f6a48fb0c9445b0974351a9b19be393")
+                .when().get()
+                .then()
+                //log().headers();
+                .log().body();
     }
 
     @Test
-    public void log_cookies_or_status() {
+    public void log_cookies_and_status() {
         given()
-                .baseUri("https://restcountries.com/v3.1")
-                .when()
-                .get("/alpha/US").
-                then()
+                .baseUri("https://restcountries.com/countries/v5")
+                .basePath("/name")
+                .queryParam("q", "USA")
+                .headers("Authorization", "Bearer rc_live_8f6a48fb0c9445b0974351a9b19be393")
+                .when().get()
+                .then()
                 .log().status();
-        //.log().cookies();
+                //.log().cookies();
     }
+
 
     @Test
     public void log_if_error() {
         given()
-                .baseUri("https://restcountries.com/v3.1").
-                when()
-                .get("/alpha/UT").
-                then()
-                //.log().ifStatusCodeIsEqualTo(404)
-                .log().ifError()
-                .statusCode(404);
+                .baseUri("https://restcountries.com/countries/v5")
+                .basePath("/name")
+                .queryParam("q","")
+                .headers("Authorization", "Bearer rc_live_8f6a48fb0c9445b0974351a9b19be393")
+                .when().get()
+                .then()
+                //.log().ifError()             //Only if error occurs
+                .log().ifStatusCodeIsEqualTo(400);  //Only if to display the occurred error code
+
     }
 
     @Test
     public void log_if_validation_fails() {
+
         given()
-                .baseUri("https://restcountries.com/v3.1").
-                when()
-                .get("/alpha/UT").
-                then()
-                .log().ifValidationFails()
-                .statusCode(404);
-    }
+                .baseUri("https://restcountries.com/countries/v5")
+                .basePath("/name")
+                .queryParam("q","USA")
+                .headers("Authorization", "Bearer rc_live_8f6a48fb0c9445b0974351a9b19be393")
+                .when().get()
+                .then()
+                .log().ifValidationFails()  //About assertions
+                //.statusCode(400);
+                .statusCode(200);
+
 
 }
+    }
