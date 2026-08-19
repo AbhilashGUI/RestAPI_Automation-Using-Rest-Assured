57368fbfddd183211cfa2ddf35bbb337e229eaca
AbhilashGUI <vemulaabhilash8433@gmail.com>
2026-08-19 15:41:24 +0530

    Online project learning

--- src/test/java/auth/Auth.java
+++ src/test/java/auth/Auth.java
@@ -21,16 +21,18 @@ public class Auth {
 
         given()
                 .baseUri("https://postman-echo.com")
-                .auth().digest("postman", "password").
-                when()
-                .get("/digest-auth").
-                then()
+                .basePath("/digest-auth")
+               //.auth().basic("postman", "password")
+                .auth().digest("postman","password")   //Passing the username and password
+                .when()
+                .get()
+                .then()
                 .log().all()
                 .statusCode(200);
     }
-
+    }
     /*
-     * oAuth 1.0
+     * oAuth 1.0: Is a kind of authentication used to work on our-behalf by allowing the 3rd party app
      *
      * Consumer Key (API Key)
      * Consumer Secret (API Secret Key)
@@ -40,25 +42,25 @@ public class Auth {
      * https://api.twitter.com/1.1/statuses/update.json
      */
 
-    @Test
-    public void post_A_Tweet() {
+    /*@Test
+   public void post_A_Tweet() {
 
         String tweet = "This is a test tweet from Rest Assured.";
 
         given()
                 .baseUri("https://api.twitter.com/1.1")
+                .basePath("/statuses/update.json")
                 .auth().oauth(
                         "uJ6haiHUTq0LEqpwfIaTwXlNT",
                         "mJJgpLtWpmvIgyE6BazJ79UJapv3N1DaI4F1PzhVTax0T4Z5GF",
                         "950475365499236352-2Vewe2vmKgzHhttyzAMXb38ohT1QjQx",
                         "yIEMjAfl8vSDoAKicuWQ0hNLu0vMrk6lzMRuK3WzecMVX")
-                .param("status", tweet).
-                when()
-                .post("/statuses/update.json").
-                then()
+                .param("status", tweet)
+                .when().post()
+                .then()
                 .log().all()
                 .statusCode(200);
-    }
+    }*/
 
 
     /*
@@ -70,38 +72,36 @@ public class Auth {
      *
      */
 
-    @Test
+   /* @Test
     public void paypal_test() {
 
         String accessToken = given()
                 .baseUri("https://api.sandbox.paypal.com/v1")
+                .basePath("/oauth2/token")
                 .contentType("application/x-www-form-urlencoded;charset=UTF-8")
                 .header("Accept-Language","en_US")
                 .param("grant_type", "client_credentials")
                 .auth().preemptive().basic(
                         "Afq8Vt73m98ez_xXDGaDIwXueTXcTvGSizt4SkOcM5J9ZeWoxFKuCI4x12kqYPsZis6mCPQLeuHhiMRg",
-                        "EBlgaZ_4UKOo6i22KAcQjVyo8hFYqySaRuawMrUmPlmHSbQJLjLOIRDdnKvQ3W5lnc-d6iulVsc9OR-z").
-                when()
-                .post("/oauth2/token").
-                then()
-                .log().all()
+                        "EBlgaZ_4UKOo6i22KAcQjVyo8hFYqySaRuawMrUmPlmHSbQJLjLOIRDdnKvQ3W5lnc-d6iulVsc9OR-z")
+                .when().post()
+                .then().log().all()
                 .statusCode(200)
                 .extract().path("access_token");
 
         //Generate next invoice
 
         given()
-                .baseUri("https://api.sandbox.paypal.com/v2")
+                .baseUri("https://api.sandbox.paypal.com")
+                .basePath("/v2/invoicing/generate-next-invoice-number")
                 .contentType("application/json")
-                .auth().oauth2(accessToken).
-                when()
-                .post("/invoicing/generate-next-invoice-number").
-                then()
-                .log().all()
+                .auth().oauth2(accessToken)
+                .when().post()
+                .then().log().all()
                 .statusCode(200);
 
 
 
-    }
-}
+    }*/
+
 
--- src/test/java/headers_and_cookies/Headers_And_Cookies.java
+++ src/test/java/headers_and_cookies/Headers_And_Cookies.java
@@ -34,114 +34,110 @@ public class Headers_And_Cookies {
 
 
     @Test
-    public void sending_response_headers() {
+    public void sending_response_headers() {   //Validing the headers in response body
+       given()
+               .baseUri("https://data.fixer.io/api")
+               .basePath("/latest")
+               .queryParam("access_key", "eaaaa33d3571fef962d994f117f202dd")
+               .queryParam("Symbols","INR")
+               .headers("etag", "16e5b3731478389e0d2c0a0ab61d4105")
+               .headers("last-modified","Wed, 19 Aug 2026 03:35:04 GMT")
+               .log().all()
+               .when().get()
+               .then().log().all()
+               .statusCode(200);
+    }
+
+    @Test
+    public void sending_headers_intheformofobject() { //Validating the headers in response body
+        HashMap<String,Object> headers=new HashMap<>();
+        headers.put("content-type","application/json; Charset=UTF-8");
+        headers.put("access-control-allow-methods","GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS");
+        headers.put("access-control-allow-origin","*");
+        headers.put("x-request-time","0.013");
+
         given()
-                .baseUri("http://data.fixer.io/api")
-                .queryParam("access_key", "eaaaa33d3571fef962d994f117f202dd")
-                .queryParam("Symbols", "INR")
-                .header("etag", "ee8d42ca86290f687b5a42ee5b8ecc07")
-                .header("last-modified", "Thu, 18 Sep 2025 15:44:05 GMT").
-                when()
-                .get("/latest").
-                then()
-                .log().all()
+                .baseUri("https://data.fixer.io/api")
+                .basePath("latest")
+                .queryParam("access_key","eaaaa33d3571fef962d994f117f202dd")
+                .queryParam("Symbols","INR")
+                .headers(headers).log().all()
+                .when().get()
+                .then().log().all()
                 .statusCode(200);
     }
 
     @Test
-    public void sending_headers_object() {
-        HashMap<String, Object> headers = new HashMap<String, Object>();
-        headers.put("etag", "ee8d42ca86290f687b5a42ee5b8ecc07");
-        headers.put("last-modified", "Thu, 18 Sep 2025 15:44:05 GMT");
-        headers.put("Accept-Encoding", "gzip, deflate, br");
-        headers.put("Connection", "keep-alive");
+    public void sending_request_cookies() {   //Validating the cookies in the response body
 
         given()
-                .baseUri("http://data.fixer.io/api")
-                .queryParam("access_key", "eaaaa33d3571fef962d994f117f202dd")
-                .queryParam("Symbols", "INR")
-                .headers(headers).
-                when()
-                .get("/latest").
-                then()
+                .baseUri("https://data.fixer.io/api")
+                .basePath("/latest")
+                .queryParam("access_key","eaaaa33d3571fef962d994f117f202dd")
+                .queryParam("Symbols","INR")
+                .cookie("Userdefined","Testcheck1","Testcheck2")
                 .log().all()
-                .statusCode(200);
-    }
-
-    @Test
-    public void sending_request_cookies() {
-        given()
-                .baseUri("http://data.fixer.io/api")
-                .queryParam("access_key", "eaaaa33d3571fef962d994f117f202dd")
-                .queryParam("Symbols", "INR")
-                .cookie("user","Test","Test1","Test2").
-                when()
-                .get("/latest").
-                then()
-                .log().all()
+                .when().get()
+                .then().log().all()
                 .statusCode(200);
     }
   //Importance of cookie builder is that we have an additional methods
     @Test
-    public void sending_cookies_usinhg_builder() {
-        Cookie cookie = new Cookie.Builder("usertype","int").setSecured(true).setComment("test cookie").build();
+    public void sending_cookies_using_builder() {  //Validating the cookies in the response body
+
+        Cookie cookie= new Cookie.Builder("Usertype","int").setSecured(true).setComment("test cookie").build();
         given()
-                .baseUri("http://data.fixer.io/api")
-                .queryParam("access_key", "eaaaa33d3571fef962d994f117f202dd")
-                .queryParam("Symbols", "USD")
-                .cookie(cookie).
-                when()
-                .get("/latest").
-                then()
-                .log().all()
+                .baseUri("https://data.fixer.io/api")
+                .basePath("/latest")
+                .queryParam("access_key","eaaaa33d3571fef962d994f117f202dd")
+                .queryParam("Symbols","INR")
+                .cookie(cookie).log().all()
+                .when().get()
+                .then().log().all()
                 .statusCode(200);
     }
 
     @Test
-    public void validate_response_header() {
-        given()
-                .baseUri("http://data.fixer.io/api")
-                .queryParam("access_key", "eaaaa33d3571fef962d994f117f202dd")
-                .queryParam("Symbols", "INR").
-                when()
-                .get("/latest").
-                then()
-                .log().all()
-                .statusCode(200)
-        .header("transfer-encoding", "chunked");
+    public void validate_response_header() {  //Sending the headers in response and Validating the same
+       given()
+               .baseUri("https://data.fixer.io/api")
+               .basePath("/latest")
+               .queryParam("access_key","eaaaa33d3571fef962d994f117f202dd")
+               .queryParam("Symbols","INR")
+               .when().get()
+               .then().log().all()
+               .statusCode(200)
+               .headers("content-type","application/json; Charset=UTF-8");
     }
 
     @Test
-    public void extract_response_header() {
-        Headers headers = given()
-                .baseUri("http://data.fixer.io/api")
-                .queryParam("access_key", "eaaaa33d3571fef962d994f117f202dd")
-                .queryParam("Symbols", "INR").
-                when()
-                .get("/latest").
-                then()
-                .log().all()
-                .statusCode(200)
-                .extract().headers();
+    public void extract_response_header() {  //Extracting the values of headers in response body
 
+        Headers headers= given()
+                .baseUri("https://data.fixer.io/api")
+                .basePath("/latest")
+                .queryParam("access_key","eaaaa33d3571fef962d994f117f202dd")
+                .queryParam("Symbols","INR")
+                .when().get()
+                .then().log().all()
+                .extract().headers();
         System.out.println(headers.getValue("date"));
         System.out.println(headers.getValue("content-type"));
-        System.out.println(headers.getValue("Transfer-Encoding"));
+        System.out.println(headers.getValue("x-request-time"));
+
+
     }
 
     @Test
     public void extract_response_cookies() {
-        Map<String,String> cookies = given()
-                .baseUri("http://data.fixer.io/api")
-                .queryParam("access_key", "eaaaa33d3571fef962d994f117f202dd")
-                .queryParam("Symbols", "INR").
-                when()
-                .get("/latest").
-                then()
-                .log().all()
-                .statusCode(200)
+       Map<String,String> cookies= given()
+                .baseUri("https://data.fixer.io/api")
+                .basePath("/latest")
+                .queryParam("access_key","eaaaa33d3571fef962d994f117f202dd")
+                .queryParam("Symbols","INR")
+                .when().get()
+                .then().log().all()
                 .extract().cookies();
-
         System.out.println(cookies.get("chunked"));
 
     }
